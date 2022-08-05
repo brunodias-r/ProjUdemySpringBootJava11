ad6781fc688496f7d225bee76e979da3756a05e6
Bruno Rodrigues <bruno.brd73@gmail.com>
2022-08-05 12:50:20 -0300

    Project created

---
+++ src/test/java/br/com/projetoaula/ProjetoAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
